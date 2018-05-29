99fe1026bd093aeecbfeafbf5a1f6d6af3413e14
Unknown <privat@julianweise.de>
2018-05-29 10:45:03 +0200

    refactor regarding PR #1 comments

--- src/tetris/field/field.ts
+++ src/tetris/field/field.ts
@@ -6,67 +6,77 @@ import BrickFactory from '../brick/brickFactory';
 
 export default class Field {
 
-    private _activeBrick: Brick;
-    private readonly _width: integer;
-    private readonly _height: integer;
-    private readonly _brickFactory: BrickFactory;
+	private _activeBrick: Brick;
+	private readonly _width: integer;
+	private readonly _height: integer;
+	private readonly _brickFactory: BrickFactory;
 
 	// contains only stuck bricks
 	private readonly _bricks: Brick[];
 	private readonly _state: boolean[][];
 
-    public constructor(width: number, height: number, brickFactory: BrickFactory) {
-        this._width = width;
-        this._height = height;
-        this._bricks = [];
-        this._brickFactory = brickFactory;
+	public constructor(width: number, height: number, brickFactory: BrickFactory) {
+		this._width = width;
+		this._height = height;
+		this._bricks = [];
+		this._brickFactory = brickFactory;
 		this._state = new Array(this._height);
 		this._setupField();
-    }
-
-    private _setupField() {
-        const iterator = this._state.keys();
-        for (let key of iterator) {
-            this._state[key] = new Array(this._width).fill(false);
-        }
-    }
-
-    private _addToField(blocks: Block[]) {
-        blocks.forEach(block => {
-			this._state[block.position.x][block.position.y] = true;
-        });
-    }
-
-    public addBrick(brick: Brick) {
-        this._bricks.push(brick);
-    }
-
-    set activeBrick(brick: Brick) {
-        this._activeBrick = brick;
-    }
-
-    get activeBrick(): Brick {
-        return this._activeBrick;
-    }
-
-    get state(): boolean[][] {
-        return this._state;
-    }
-
-    get bricks(): Brick[] {
-    	return this._bricks;
 	}
 
-    public update(time: number, delta: number) {
-        if (!this.activeBrick) {
-            this.activeBrick = this._brickFactory.newBrick(this);
-        }
-        this._activeBrick.update(time, delta);
-        if (this.activeBrick.isStuck()) {
-            this._addToField(this._activeBrick.blocks);
-            this.addBrick(this.activeBrick);
-            this.activeBrick = null;
-        }
-    }
+	private _setupField(): void {
+		const iterator = this._state.keys();
+		for (let key of iterator) {
+			this._state[key] = new Array(this._width).fill(false);
+		}
+	}
+
+	private _addToField(blocks: Block[]): void {
+		blocks.forEach(block => {
+			this._state[block.position.x][block.position.y] = true;
+		});
+	}
+
+	public update(time: number, delta: number): void {
+		if (!this.activeBrick) {
+			this.activeBrick = this._brickFactory.newBrick(this);
+		}
+		this._activeBrick.update(time, delta);
+		if (this.activeBrick.isStuck()) {
+			this._addToField(this._activeBrick.blocks);
+			this.addBrick(this.activeBrick);
+			this.activeBrick = null;
+		}
+	}
+
+	// Accessors
+
+	public addBrick(brick: Brick): void {
+		this._bricks.push(brick);
+	}
+
+	public set activeBrick(brick: Brick) {
+		this._activeBrick = brick;
+	}
+
+	public get activeBrick(): Brick {
+		return this._activeBrick;
+	}
+
+	public get state(): boolean[][] {
+		return this._state;
+	}
+
+	public get bricks(): Brick[] {
+		return this._bricks;
+	}
+
+	public get width(): number {
+		return this._width;
+	}
+
+	public get height(): number {
+		return this._height;
+	}
 
 }
--- src/tetris/scenes/mainScene.ts
+++ src/tetris/scenes/mainScene.ts
@@ -1,18 +1,17 @@
 /// <reference path="../../../definitions/phaser.d.ts"/>
 
-
 import Field from "../field/field";
 import Player from "../player/player";
 import BrickFactory from "../brickFactory/brickFactory";
 
-const GAME_WIDTH: number = 800;
-const GAME_HEIGHT: number = 600;
+const GAME_WIDTH: number = 18;
+const GAME_HEIGHT: number = 10;
 
 export class MainScene extends Phaser.Scene {
+
 	private phaserSprite: Phaser.GameObjects.Sprite;
 	private player: Player;
 	private field: Field;
-
 
 	constructor() {
 		super({
@@ -22,15 +21,15 @@ export class MainScene extends Phaser.Scene {
 		this.field = new Field(GAME_WIDTH, GAME_HEIGHT, new BrickFactory());
 	}
 
-	preload(): void {
+	public preload(): void {
 		this.load.image("logo", "./assets/images/phaser.png");
 	}
 
-	create(): void {
+	public create(): void {
 		this.phaserSprite = this.add.sprite(400, 300, "logo");
 	}
 
-	update(time: number, interval: number) {
+	public update(time: number, interval: number): void {
 
 	}
 }
