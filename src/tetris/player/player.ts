2238c1c7c028f7991c0398d7b621f9b2a823601b
Unknown <privat@julianweise.de>
2018-05-29 12:49:48 +0200

    Implement abstract player

--- src/tetris/player/player.ts
+++ src/tetris/player/player.ts
@@ -2,13 +2,41 @@ import Field from '../field/field';
 
 export default abstract class Player {
 
-	private _field: Field;
+	//region public members
+	//endregion
 
+	//region public methods
+	public abstract update(time: number, delta: number): void;
+	//endregion
+
+	//region constructor
 	protected constructor(field: Field) {
 		this._field = field;
 	}
+	//endregion
 
-	update(time: number, delta: number) {
+	//region private members
+	private _field: Field;
+	//endregion
 
+	//region private methods
+	//endregion
+
+	//region protected methods
+	protected moveLeft(): void {
+		this._field.moveBrickLeft();
 	}
+
+	protected moveRight(): void {
+		this._field.moveBrickRight();
+	}
+
+	protected moveDown(): void {
+		this._field.moveBrickDown();
+	}
+
+	protected drop(): void {
+		this._field.dropBrick();
+	}
+	//endregion
 }
