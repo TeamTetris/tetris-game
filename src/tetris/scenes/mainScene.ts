be306de0be7013d412aeb8a2130e694e45fcf489
Unknown <privat@julianweise.de>
2018-05-29 10:22:10 +0200

    add field to main game scene

--- src/tetris/scenes/mainScene.ts
+++ src/tetris/scenes/mainScene.ts
@@ -1,10 +1,25 @@
+/// <reference path="../../../definitions/phaser.d.ts"/>
+
+
+import Field from "../field/field";
+import Player from "../player/player";
+import BrickFactory from "../brickFactory/brickFactory";
+
+const GAME_WIDTH: number = 800;
+const GAME_HEIGHT: number = 600;
+
 export class MainScene extends Phaser.Scene {
 	private phaserSprite: Phaser.GameObjects.Sprite;
+	private player: Player;
+	private field: Field;
+
 
 	constructor() {
 		super({
 			key: "MainScene"
 		});
+		this.player = new Player();
+		this.field = new Field(GAME_WIDTH, GAME_HEIGHT, new BrickFactory());
 	}
 
 	preload(): void {
