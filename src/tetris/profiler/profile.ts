7a91e3ca3e6124bb5b194bf6936c12b5fbbf776d
Julian Weise <privat@julianweise.de>
2018-05-31 12:32:28 +0200

    add boilerplate profile implementation

---
+++ src/tetris/profiler/profile.ts
@@ -0,0 +1,19 @@
+export default class Profile {
+	//region public members
+	public add(name: string, data: object) {
+		// TODO
+	}
+	//endregion
+
+	//region public methods
+	//endregion
+
+	//region constructor
+	//endregion
+
+	//region private members
+	//endregion
+
+	//region private methods
+	//endregion
+}
