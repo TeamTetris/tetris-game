0d9efb27c8e7c7af754735a5bad1b0401173d93d
Julian Weise <privat@julianweise.de>
2018-06-06 10:01:45 +0200

    add geoLocationService to fetch and convert geoLocation information

---
+++ src/tetris/profiler/service/geoLocationService.ts
@@ -0,0 +1,127 @@
+import ProfileData from "tetris/profiler/profileData";
+import GeoLocationData from "tetris/profiler/geoLocationData";
+import InverseGeoCordingService from "tetris/profiler/service/inverseGeoCordingService";
+
+const IP_GEO_SERVICE = 'https://ipapi.co/json/';
+
+export default class GeoLocationService {
+
+	//region public members
+	//endregion
+
+	//region public methods
+	public requestCurrentLocation(): void {
+		if (!GeoLocationService._browserIsCompatible()) {
+			// fallback handling: Try to get position based on IP
+			this._calculateLocationBasedOnIP();
+		}
+		navigator.geolocation.getCurrentPosition(
+			this._inverseGeoCordingService.convert.bind(
+				this._inverseGeoCordingService,
+				this._receiveLocation,
+				this._receiveLocationError
+			),
+			this._receiveLocationError
+		);
+	}
+
+	public monitorCurrentLocation(): void {
+		if (!GeoLocationService._browserIsCompatible()) {
+			// fallback handling: Try to get position based on IP
+			this._calculateLocationBasedOnIP();
+		}
+		navigator.geolocation.watchPosition(
+			this._inverseGeoCordingService.convert.bind(
+				this._inverseGeoCordingService,
+				this._receivePeriodicLocation,
+				this._receiveLocationError
+			),
+			this._receiveLocationError
+		);
+	}
+
+	public stopMonitoring(): void {
+		if(this._monitoringId == null) {
+			console.log(Error('No monitoring active'));
+			return;
+		}
+		navigator.geolocation.clearWatch(this._monitoringId);
+		this._monitoringId = null;
+	}
+	//endregion
+
+	//region constructor
+	constructor(successCallback: (ProfileData) => void, errorCallback: (Error) => void) {
+		this._successCallback = successCallback;
+		this._errorCallback = errorCallback;
+		this._inverseGeoCordingService = new InverseGeoCordingService();
+	}
+	//endregion
+
+	//region private members
+	private _monitoringId: number;
+	private readonly _successCallback: (_:ProfileData) => void;
+	private readonly _errorCallback: (_:Error) => void;
+	private readonly _inverseGeoCordingService: InverseGeoCordingService;
+	//endregion
+
+	//region private methods
+	private static _browserIsCompatible(): boolean {
+		return !!navigator.geolocation;
+	}
+
+	private _receiveLocation(resultSet: object): void {
+		if(resultSet['results'].length < 1) {
+			this._calculateLocationBasedOnIP();
+			return;
+		}
+		let zip, city, country;
+		const address_components = resultSet['results'][0]['address_components'];
+		address_components.forEach(component => {
+			if (component['types'].contains('postal_code')) {
+				zip = component['long_name']
+			}
+			if (component['types'].contains('country')) {
+				country = component['long_name']
+			}
+			if (component['types'].contains('locality')) {
+				city = component['long_name']
+			}
+		});
+		this._successCallback(new ProfileData(new GeoLocationData(zip, city, country)));
+	}
+
+	private _receivePeriodicLocation(position: Position): void {
+		// Implement different behavior for periodic position request
+		this._receiveLocation(position);
+	}
+
+	private _receiveLocationError(locationError: PositionError) {
+		console.log(locationError);
+		// fallback handling: Try to get position based on IP
+		this._calculateLocationBasedOnIP();
+	}
+
+	private _calculateLocationBasedOnIP(): void {
+		fetch(IP_GEO_SERVICE)
+			.then((response) => {
+				return response.json();
+			})
+			.then((data) => {
+				this._receiveIPLocation(data['postal'], data['city'], data['country_name']);
+			})
+			.catch((error) => {
+				this._handleIPLocationError(error);
+			});
+	}
+
+	private _receiveIPLocation(zip: number, city: string, country: string): void {
+		this._successCallback(new ProfileData(new GeoLocationData(zip, city, country)));
+	}
+
+	private _handleIPLocationError(error: Error) {
+		console.log(error);
+		this._errorCallback(error);
+	}
+	//endregion
+}
---
+++ src/tetris/profiler/service/inverseGeoCordingService.ts
@@ -0,0 +1,50 @@
+import GoogleMapsAPI from 'googlemaps';
+
+export default class InverseGeoCordingService {
+
+	//region public members
+	//endregion
+
+	//region public methods
+	public convert(successCallback: (object) => void, errorCallback: (Error) => void, location: Position): void {
+		const reverseGeocodeParams = {
+			"latlng":        location.coords.latitude + "," + location.coords.longitude,
+			"result_type":   "postal_code",
+			"language":      "en",
+			"location_type": "APPROXIMATE"
+		};
+
+		this._googleMapsAPI.reverseGeocode(reverseGeocodeParams, function(err, result){
+			console.log(result);
+			if(result) {
+				successCallback(result);
+			}
+			else {
+				errorCallback(err);
+			}
+		});
+	}
+	//endregion
+
+	//region constructor
+	constructor() {
+		const publicConfig = {
+			key: '<INSERT API KEY>',
+			stagger_time:       1000, // for elevationPath
+			encode_polylines:   false,
+			secure:             false // use https
+		};
+		this._googleMapsAPI = new GoogleMapsAPI(publicConfig);
+	}
+	//endregion
+
+	//region constructor
+	//endregion
+
+	//region private members
+	private _googleMapsAPI: GoogleMapsAPI;
+	//endregion
+
+	//region private methods
+	//endregion
+}
