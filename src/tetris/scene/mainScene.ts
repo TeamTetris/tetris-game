848346a89661c83fd824a9588f739d06b388b3a5
Johannes Schneider <johannes.schneider@student.hpi.de>
2018-06-08 10:48:35 +0200

    add field based score.

--- src/tetris/scene/mainScene.ts
+++ src/tetris/scene/mainScene.ts
@@ -11,6 +11,7 @@ const FIELD_WIDTH: number = 10;
 const FIELD_HEIGHT: number = 18;
 const FIELD_DRAW_OFFSET: Vector2 = new Vector2(240, 0);
 const BLOCK_SIZE: number = 32;
+const FONT_STYLE: object = {font: "20px Kenney Mini Square", fill: "#fff"};
 
 export default class MainScene extends Phaser.Scene {
 
@@ -24,7 +25,9 @@ export default class MainScene extends Phaser.Scene {
 
 	public create(): void {
 		this._createFieldBackground();
-		this._player = new LocalPlayer(this._field, this.input.keyboard, this._biasEngine.newEventReceiver());		
+
+		this._field = this._newField(FIELD_WIDTH, FIELD_HEIGHT, FIELD_DRAW_OFFSET);
+		this._player = new LocalPlayer(this._field, this.input.keyboard, this._biasEngine.newEventReceiver());
 	}
 
 	public update(time: number, delta: number): void {
@@ -39,12 +42,13 @@ export default class MainScene extends Phaser.Scene {
 			key: "MainScene"
 		});
 		this._biasEngine = biasEngine;
-		this._field = new Field(FIELD_WIDTH, FIELD_HEIGHT, FIELD_DRAW_OFFSET, new BrickFactory(this, biasEngine));
+		this._brickFactory = new BrickFactory(this, biasEngine);
 	}
 	//endregion
 
 	//region private members
-	private _biasEngine: BiasEngine;
+	private readonly _biasEngine: BiasEngine;
+	private readonly _brickFactory: BrickFactory;
 	private _player: Player;
 	private _field: Field;
 	private _fieldBackground: Phaser.GameObjects.Graphics;
@@ -55,6 +59,11 @@ export default class MainScene extends Phaser.Scene {
 		this._fieldBackground = this.add.graphics();
 		this._fieldBackground.fillStyle(0x004070);
 		this._fieldBackground.fillRect(FIELD_DRAW_OFFSET.x, FIELD_DRAW_OFFSET.y, BLOCK_SIZE * FIELD_WIDTH, BLOCK_SIZE * FIELD_HEIGHT);
+	}
+
+	private _newField(fieldWidth: number, fieldHeight: number, drawOffset: Vector2): Field {
+		const scoreText = this.add.text(drawOffset.x, drawOffset.y, "0", FONT_STYLE);
+		return new Field(fieldWidth, fieldHeight, drawOffset, scoreText, this._brickFactory);
 	}
 	//endregion
 }
