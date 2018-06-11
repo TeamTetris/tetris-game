224bb6667d702c4a5d1f0adf60f8f2cfa259f4c7
Johannes Schneider <johannes.schneider@student.hpi.de>
2018-06-11 18:08:36 +0200

    add usage example for profile data update strategy.

--- src/tetris/profiler/profile.ts
+++ src/tetris/profiler/profile.ts
@@ -1,18 +1,20 @@
 import BaseProfileData from 'tetris/profiler/baseProfileData';
 import ProfileData from 'tetris/profiler/profileData';
 import GeoLocation from 'tetris/profiler/profileValues/geoLocation';
+import NumberDataUpdateStrategy from "tetris/profiler/updateStrategy/numberDataUpdateStrategy";
 
 const LOCATION_KEY = "location";
+const AGE_KEY = "age";
 
 export default class Profile {
 
 	//region public members
 	public get location(): ProfileData<GeoLocation> {
-		if (!this._data.has(LOCATION_KEY)) {
-			throw new Error("cannot read location data from profile!")
-		}
+		return this._getProperty(LOCATION_KEY) as ProfileData<GeoLocation>;
+	}
 
-		return this._data.get(LOCATION_KEY) as ProfileData<GeoLocation>;
+	public get age(): ProfileData<number> {
+		return this._getProperty(AGE_KEY) as ProfileData<number>;
 	}
 	//endregion
 
@@ -22,7 +24,8 @@ export default class Profile {
 	//region constructor
 	public constructor() {
 		this._data = new Map<string, BaseProfileData>();
-		this._addProfileData<GeoLocation>(LOCATION_KEY);
+		this._newProfileData<GeoLocation>(LOCATION_KEY);
+		this._newProfileData<number>(AGE_KEY).updateStrategy = new NumberDataUpdateStrategy();
 	}
 	//endregion
 
@@ -31,8 +34,19 @@ export default class Profile {
 	//endregion
 
 	//region private methods
-	private _addProfileData<ValueType>(name: string): void {
-		this._data.set(name, new ProfileData<ValueType>());
+	private _getProperty(key: string): BaseProfileData {
+		if (!this._data.has(key)) {
+			throw new Error("cannot read " + key + " data from profile!")
+		}
+
+		return this._data.get(key);
+	}
+
+	private _newProfileData<ValueType>(name: string): ProfileData<ValueType> {
+		const profileData = new ProfileData<ValueType>();
+		this._data.set(name, profileData);
+
+		return profileData;
 	}
 	//endregion
 }
