15c636641646a4d0d42bd3eff7fc45ff89659821
Christian Friedow <christian@friedow.com>
2018-06-21 14:16:27 +0200

    Renamed sprite file and extracted values to config

---
+++ src/tetris/config.ts
@@ -0,0 +1,16 @@
+const config = {
+    sceneKeys: {
+        mainScene: 'MainScene',
+        menuScene: 'MenuScene',
+    },
+    atlasKeys: {
+        uiSpriteAtlasKey: "uiSpriteAtlas",
+        blockSpriteAtlasKey: "blockSpriteAtlas",
+    },
+    ui: {
+        spacing: 20,
+    },
+    defaultFontStyle: {font: "20px Kenney Mini Square", fill: "#fff"},
+}
+
+export default config;
