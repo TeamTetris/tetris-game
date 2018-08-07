c6ec4df81f6453849f4d5cfeadc3e0b35a4f308a
Christian Friedow <christian@friedow.com>
2018-08-07 13:46:07 +0200

    Refactored scoreboard widget

--- src/tetris/ui/scoreboardWidget.ts
+++ src/tetris/ui/scoreboardWidget.ts
@@ -8,6 +8,8 @@ export interface Font {
     font: {},
 }
 
+const DISPLAYED_PLAYERS = 8;
+
 export default class ScoreboardWidget {
 
     //region public members
@@ -41,55 +43,31 @@ export default class ScoreboardWidget {
 	}
 
 	set x(x: number) {
-        const offset = x - this._x;
-        for (let index = 0; index < this.ranks.length; index++) {
-            this.ranks[index].x += offset;
-            this.names[index].x += offset;
-            this.scores[index].x += offset;
-        }
-        for (const divider of this.dividers) {
-            divider.x += offset;
-        }
         this._x = x;
+        this._adjustTextX();
+        this._adjustDividerX();
 	}
 
 	set y(y: number) {
-        const offset = y - this._y;
-		for (let index = 0; index < this.ranks.length; index++) {
-            this.ranks[index].y += offset;
-            this.names[index].y += offset;
-            this.scores[index].y += offset;
-        }
-        for (const divider of this.dividers) {
-            divider.y += offset;
-        }
         this._y = y;
+        this._adjustTextY();
+        this._adjustDividerY();
 	}
 
-	public update(playerScores: MatchPlayer[]): void {
-		this._clearScoreboard();
-
-		for (const [index, player] of playerScores.entries()) {
-            
-            this._createPlayerText(index, player);
-
-			if (index > 0) {
-                const currentPlayerRank = parseInt(player.rank, 10);
-                const previousPlayerRank = parseInt(playerScores[index - 1].rank, 10);
-                const doubleLine = currentPlayerRank - previousPlayerRank > 1;
-                const lineColor = player.scoreboardStatus === ScoreboardStatus.Endangered ? config.ui.colors.red.hex : config.ui.colors.white.hex;
-                this._createDivider(index, lineColor, doubleLine);
-            }
-		}
+	public update(players: MatchPlayer[]): void {
+        this._updatePlayerText(players);
+        this._updateDividers(players);
 	}
 	//endregion
 
 	//region constructor
 	public constructor(scene: Phaser.Scene, x: number, y: number, font: Font = config.ui.fonts.scoreboard) {
         this._scene = scene;
-		this.x = x;
-        this.y = y;
         this._font = font;
+        this._createTextObjects();
+        this._createDividers();
+        this.x = x;
+        this.y = y;
 	}
 	//endregion
 
@@ -101,59 +79,103 @@ export default class ScoreboardWidget {
 	//endregion
 
     //region private methods
-    private _clearScoreboard(): void {
-        for (let index = 0; index < this.ranks.length; index++) {
-            this.ranks[index].destroy();
-            this.names[index].destroy();
-            this.scores[index].destroy();
-        }
-        this.ranks.splice(0, this.ranks.length - 1);
-        this.names.splice(0, this.names.length - 1);
-        this.scores.splice(0, this.scores.length - 1);
-    }
-
-    private _createPlayerText(index: number, player: MatchPlayer): void {
-        // Add rank text
-        const rank = this._scene.add.text(0, 0, player.rank, this._font.font);
-        rank.x = this.x - 125 - rank.width / 2;
-        rank.y = this.y + index * (config.ui.spacing + this._font.size);
-
-        // Add name text
-        const name = this._scene.add.text(0, 0, player.name, this._font.font);
-        name.x = this.x - name.width / 2;
-        name.y = this.y + index * (config.ui.spacing + this._font.size);
-
-        // Add score text
-        const score = this._scene.add.text(0, 0, player.score, this._font.font);
-        score.x = this.x + 125 - score.width / 2;
-        score.y = this.y + index * (config.ui.spacing + this._font.size);
-
-        // Set text color
-        if (player.scoreboardStatus === ScoreboardStatus.Endangered) {
-            rank.setColor(config.ui.colors.red.string);
-            name.setColor(config.ui.colors.red.string);
-            score.setColor(config.ui.colors.red.string);
-        } else if (player.scoreboardStatus === ScoreboardStatus.Spotlighted) {
-            rank.setPipeline('rainbow-text');
-            name.setPipeline('rainbow-text');
-            score.setPipeline('rainbow-text');
-            // TODO: Compare username
-        }else if (player.name === "You") {
-            rank.setColor(config.ui.colors.yellow.string);
-            name.setColor(config.ui.colors.yellow.string);
-            score.setColor(config.ui.colors.yellow.string);
+    private _createTextObjects(): void {
+        for (let index = 0; index < DISPLAYED_PLAYERS; index++) {
+            this.ranks.push(this._scene.add.text(0, 0, "", this._font.font));
+            this.names.push(this._scene.add.text(0, 0, "", this._font.font));
+            this.scores.push(this._scene.add.text(0, 0, "", this._font.font)); 
         }
     }
 
-    private _createDivider(index: number, color: number, double: boolean = false): void {
-        // Create graphic
-        const divider = this._scene.add.graphics();
-        const dividerY = this.y + index * (config.ui.spacing + this._font.size) - config.ui.spacing / 2;
-        divider.lineStyle(1, color, 0.4);
-        divider.lineBetween(this.x - 125, dividerY, this.x + 125, dividerY);
+    private _createDividers(): void {
+        for (let index = 1; index < DISPLAYED_PLAYERS; index++) {
+            this.dividers.push(this._scene.add.graphics());
+        }
+    }
+
+    private _updateDividers(players: MatchPlayer[]): void {
+        for (let index = 1; index < DISPLAYED_PLAYERS; index++) {
+            if (index >= players.length) {
+                return;
+            }
+            const currentPlayerRank = players[index].placement;
+            const previousPlayerRank = players[index - 1].placement;
+            const doubleLine = currentPlayerRank - previousPlayerRank > 1;
+            const lineColor = players[index].scoreboardStatus === ScoreboardStatus.Endangered ? config.ui.colors.red.hex : config.ui.colors.white.hex;
+            this._updateDivider(index, lineColor, doubleLine);
+        }
+    }
+
+    private _updateDivider(index: number, color: number, double: boolean = false): void {
+        // Clear graphic
+        this.dividers[index].clear();
+        this.dividers[index].lineStyle(1, color, 0.4);
+        this.dividers[index].lineBetween(-125, 0, 125, 0);
 
         if (double) {
-            divider.lineBetween(this.x - 125, dividerY + 2, this.x + 125, dividerY + 2);
+            this.dividers[index].lineBetween(-125, 2, 125, 2);
+        }
+    }
+
+    private _updatePlayerText(players: MatchPlayer[]): void {
+        for (let index = 0; index < DISPLAYED_PLAYERS; index++) {
+            if (index >= players.length) {
+                break;
+            }
+            this.ranks[index].setText(players[index].placement.toString());
+            this.names[index].setText(players[index].displayName);
+            this.scores[index].setText(players[index].points.toString());
+
+            // Set text color
+            if (players[index].scoreboardStatus === ScoreboardStatus.Endangered) {
+                this.ranks[index].setColor(config.ui.colors.red.string);
+                this.names[index].setColor(config.ui.colors.red.string);
+                this.scores[index].setColor(config.ui.colors.red.string);
+            } else if (players[index].scoreboardStatus === ScoreboardStatus.Spotlighted) {
+                this.ranks[index].setPipeline('rainbow-text');
+                this.names[index].setPipeline('rainbow-text');
+                this.scores[index].setPipeline('rainbow-text');
+            // } else if (players[index].scoreboardStatus === ScoreboardStatus.You) {
+            //     this.ranks[index].setColor(config.ui.colors.yellow.string);
+            //     this.names[index].setColor(config.ui.colors.yellow.string);
+            //     this.scores[index].setColor(config.ui.colors.yellow.string);
+            } else {
+                this.ranks[index].setColor(config.ui.colors.white.string);
+                this.names[index].setColor(config.ui.colors.white.string);
+                this.scores[index].setColor(config.ui.colors.white.string);
+            }
+        }
+        this._adjustTextX();
+    }
+
+    
+
+    private _adjustTextX(): void {
+        for (let index = 0; index < DISPLAYED_PLAYERS; index++) {
+            this.ranks[index].x = this.x - 125 - this.ranks[index].width / 2;
+            this.names[index].x = this.x - this.names[index].width / 2;
+            this.scores[index].x = this.x + 125 - this.scores[index].width / 2;
+        }
+    }
+
+    private _adjustTextY(): void {
+        for (let index = 0; index < DISPLAYED_PLAYERS; index++) {
+            this.ranks[index].y = this.y + index * (config.ui.spacing + this._font.size);
+            this.names[index].y = this.y + index * (config.ui.spacing + this._font.size);
+            this.scores[index].y = this.y + index * (config.ui.spacing + this._font.size);
+        }
+    }
+
+    private _adjustDividerX(): void {
+        for (const [index, divider] of this.dividers.entries()) {
+            divider.x = this.x;
+        }
+    }
+
+    private _adjustDividerY(): void {
+        for (const [index, divider] of this.dividers.entries()) {
+            const y = this.y + index * (config.ui.spacing + this._font.size) - config.ui.spacing / 2;
+            divider.y = y;
         }
     }
 	//endregion
